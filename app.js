5ae3f2fc9f744d16fa819e81b556eb0b859f63d8
syeef <syeefislam@yahoo.com>
2023-09-19 08:09:01 +0530

    some bug fix

--- app.js
+++ app.js
@@ -3,18 +3,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 dotenv.config();
-const sequelize = require("./util/database");
+
 const helmet = require("helmet");
 const morgan = require("morgan");
 const path = require("path");
 const fs = require("fs");
 const mongoose = require("mongoose");
-
-// const Expense = require("./models/expenseModel");
-// const User = require("./models/userModel");
-// const Order = require("./models/orderModel");
-// const forgotPassword = require("./models/forgotPasswordModel");
-// const downloadExpense = require("./models/expenseDownloadModel");
 
 const userRoute = require("./routes/userRoute");
 const expenseRoute = require("./routes/expenseRoute");
@@ -22,12 +16,10 @@ const purchaseRoute = require("./routes/purchaseRoute");
 const premiuemRoute = require("./routes/premiuemRoute");
 const passwordRoute = require("./routes/passwordRoute");
 
-
-
 const app = express();
 
 //app.use(helmet());    //disabled as axios js not loading
-//app.use(helmet.hidePoweredBy());   
+//app.use(helmet.hidePoweredBy());
 // app.use(                                      //axios and bootstrap is loading in localhost
 //   helmet.contentSecurityPolicy({
 //     directives: {
@@ -57,43 +49,19 @@ app.use("/purchase", purchaseRoute);
 app.use("/premiuem", premiuemRoute);
 app.use("/password", passwordRoute);
 
-
-app.use((req,res)=>{
+app.use((req, res) => {
   //console.log('url',req.url);
-  res.sendFile(path.join(__dirname,`./public/${req.url}`));
+  res.sendFile(path.join(__dirname, `./public/${req.url}`));
   //res.sendFile(`public/${req.url}`, { root: __dirname });
 });
 
-//-----
-// User.hasMany(Expense);
-// Expense.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
-
-// User.hasMany(Order);
-// Order.belongsTo(User);
-
-// User.hasMany(forgotPassword);
-// forgotPassword.belongsTo(User);
-
-// User.hasMany(downloadExpense);
-// downloadExpense.belongsTo(User);
-
-// sequelize
-//   .sync()
-//   //.sync({force:true})
-//   .then((result) => {
-//     app.listen(process.env.PORT || 3000);
-//   })
-//   .catch((err) => console.log(err));
-//-----
-
-mongoose.connect(process.env.MONGODB_URI).then(result=>{
-  //console.log(result);
-  console.log("mongodb connected");
-  app.listen(process.env.PORT || 3000);
-}).catch(err=>{
-  console.log(err);
-});
- 
-;
-
-
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then((result) => {
+    //console.log(result);
+    console.log("mongodb connected");
+    app.listen(process.env.PORT || 3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,28 +1,29 @@
+const mongoUserService = require("../services/mongoUserService");
 
-const mongoUserService  = require("../services/mongoUserService");
-
-const { validationResult } = require('express-validator');
+const { validationResult } = require("express-validator");
 
 exports.postSignup = async (req, res, next) => {
-  try{
-
-    const { name,email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
     if (!name || !email || !password) {
-      return res.status(400).json({ message: "Name ,Email and Password are required" });
+      return res
+        .status(400)
+        .json({ message: "Name ,Email and Password are required" });
     }
 
-
-      const user = await mongoUserService.createUser(req.body);
-      if(user){
-        res.status(201).json({ message: "Sign Up Succesful", success: true });
-      }else{
-        res.status(401).json({ message: "User Allready Exist with same email", success: false });
-      } 
-  }catch(error){
+    const user = await mongoUserService.createUser(req.body);
+    if (user) {
+      res.status(201).json({ message: "Sign Up Succesful", success: true });
+    } else {
+      res.status(401).json({
+        message: "User Allready Exist with same email",
+        success: false,
+      });
+    }
+  } catch (error) {
     console.log(error);
     res.status(500).json({ error: error.message });
   }
- 
 };
 
 // exports.generateAccessToken = (id, name, ispremiuemuser) => {
@@ -31,32 +32,26 @@ exports.postSignup = async (req, res, next) => {
 // };
 
 exports.postLogin = async (req, res, next) => {
-  try{
+  try {
+    const { username, password } = req.body;
+    if (!username && !password) {
+      return res
+        .status(400)
+        .json({ message: "Username and password are required" });
+    }
 
-  const { username, password } = req.body;
-  if (!username || !password) {
-    return res.status(400).json({ message: "Username and password are required" });
-  }
-
-    
     const user = await mongoUserService.loginUser(req.body);
-      if(user){
-        if(user.success){
-          res.status(200).json(user);
-        }else{
-          res.status(401).json(user);
-        } 
-      }else{
-        res.status(404).json({ message: "User Does not exist", success: false });
-      } 
-  }catch(error){
+    if (user) {
+      if (user.success) {
+        res.status(200).json(user);
+      } else {
+        res.status(401).json(user);
+      }
+    } else {
+      res.status(404).json({ message: "User Does not exist", success: false });
+    }
+  } catch (error) {
     console.log(error);
     res.status(500).json({ error: error.message });
   }
-  
 };
-
-
-
-
-
